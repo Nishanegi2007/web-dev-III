bd22869c2aad678f4de120a79dbdebca96df5f74
Nishanegi2007 <nisha12509@gmail.com>
2026-09-23 09:55:27 +0530

    Commiting today's lesson

--- lecture22/students.mongodb.js
+++ lecture22/students.mongodb.js
@@ -1,4 +1,4 @@
-// use("collageDB");
+use("collageDB");
 
 // db.createCollection("students2");
 
@@ -13,9 +13,44 @@
 
 // db.students.find({marks:{$gte:80}});
 
+//and case
+
 // db.students2.find({city:"surat",semester:{$gte:3}});
+
+//or case
+
+// db.students2.find({
+//     $or:[
+//         {city:"Surat"},{semester:{$gte:3}}
+//     ]
+// });
+
+//only to get specific attributes---projection
+
+// db.students2.find({},{name:1,semseter:1,course:1,_id:0});
+
+//ascending order
+
+// db.students2.find().sort({age:1});
+
+//desending order
+
+// db.students2.find().sort({age:-1});
+
+//limiting 
+
+// db.students2.find().limit(2);
+
+//skiping
+
+// db.students.find().skip(0);
+
+//skipping and limiting
+
+// db.students2.find().skip(1).limit(1);
+
 db.students2.find({
     $or:[
-        {city:"Surat"},{semester:{$gte:3}}
+        {semester:3},{course:"Btech"}
     ]
-});
+}).sort({age:-1}).skip(0).limit(2);
